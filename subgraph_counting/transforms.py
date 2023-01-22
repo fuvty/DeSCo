692828b12315981fd2394d48245a413beaef7665
futy18 <fuvty@outlook.com>
2023-01-22 08:24:54 +0000

    [update] add relabelByDegree in load data

--- subgraph_counting/transforms.py
+++ subgraph_counting/transforms.py
@@ -276,7 +276,7 @@ def to_homogeneous_edge_index(
 
 
 def to_device(data, device):
-    if isinstance(data, pyg.data.Data) or isinstance(data, pyg.data.HeteroData):
+    if isinstance(data, Data) or isinstance(data, HeteroData):
         # pyg data batch
         return data.to(device)
     elif isinstance(data, tuple):
@@ -294,7 +294,7 @@ def get_truth(data):
     """
     get ground truth count from data
     """
-    if isinstance(data, pyg.data.Data) or isinstance(data, pyg.data.HeteroData):
+    if isinstance(data, Data) or isinstance(data, HeteroData):
         return data.y
     elif isinstance(data, tuple):
         return data[3].squeeze(dim=1)  # label
@@ -306,7 +306,7 @@ def NetworkxToHetero(
     nx_graph: Union[nx.Graph, nx.DiGraph],
     type_key: str = "type",
     feat_key: str = "feat",
-) -> pyg.data.HeteroData:
+) -> HeteroData:
     """
     input: networkx graph
     node_types in networkx graph
@@ -315,7 +315,7 @@ def NetworkxToHetero(
     """
 
     nx_graph = nx_graph.to_directed()
-    hetero_graph = pyg.data.HeteroData()
+    hetero_graph = HeteroData()
 
     hetero_edge_dict = defaultdict(
         list
@@ -399,36 +399,121 @@ def NetworkxToHetero(
 
 
 class Relabel(pyg.transforms.BaseTransform):
-    r"""Performs tensor device conversion, either for all attributes of the
-    :obj:`~torch_geometric.data.Data` object or only the ones given by
-    :obj:`attrs`.
-
-    Args:
-        device (torch.device): The destination device.
-        attrs (List[str], optional): If given, will only perform tensor device
-            conversion for the given attributes. (default: :obj:`None`)
-        non_blocking (bool, optional): If set to :obj:`True` and tensor
-            values are in pinned memory, the copy will be asynchronous with
-            respect to the host. (default: :obj:`False`)
-    """
+    r"""Relabel the nodes in the graph to consecutive integers."""
 
     def __init__(
         self,
-        device: Union[int, str],
         non_blocking: bool = False,
         mode: str = "decreasing_degree",
     ):
-        self.device = device
         self.non_blocking = non_blocking
-        self.mode = mode.replace("_", " ")
+        self.mode = mode
 
-    def __call__(self, data: pyg.data.Data):
-        graph_nx = pyg.utils.to_networkx(data, to_undirected=False)
-        # graph_nx = relabel_graph_nx(graph_nx, mode= self.mode)
-        return from_networkx_reorder(graph_nx, mode=self.mode)
+    def __call__(self, data: Data):
+        mode = self.mode.replace("_", " ")
+
+        graph_nx = pyg.utils.to_networkx(data, node_attrs=["x"], to_undirected=False)
+        return from_networkx_reorder(graph_nx, group_node_attrs=["x"], mode=mode)
 
     def __repr__(self):
         return f"{self.__class__.__name__}({self.mode})"
+
+
+def from_networkx_reorder(
+    G,
+    group_node_attrs: Optional[Union[List[str], all]] = None,
+    group_edge_attrs: Optional[Union[List[str], all]] = None,
+    mode="sorted",
+) -> Data:
+    r"""Converts a :obj:`networkx.Graph` or :obj:`networkx.DiGraph` to a
+    :class:`torch_geometric.data.Data` instance.
+
+    Args:
+        G (networkx.Graph or networkx.DiGraph): A networkx graph.
+        group_node_attrs (List[str] or all, optional): The node attributes to
+            be concatenated and added to :obj:`data.x`. (default: :obj:`None`)
+        group_edge_attrs (List[str] or all, optional): The edge attributes to
+            be concatenated and added to :obj:`data.edge_attr`.
+            (default: :obj:`None`)
+
+    .. note::
+
+        All :attr:`group_node_attrs` and :attr:`group_edge_attrs` values must
+        be numeric.
+    """
+    import networkx as nx
+
+    G = nx.convert_node_labels_to_integers(G, ordering=mode)
+    G = G.to_directed() if not nx.is_directed(G) else G
+
+    if isinstance(G, (nx.MultiGraph, nx.MultiDiGraph)):
+        edges = sorted(list(G.edges(keys=False)))
+    else:
+        edges = sorted(list(G.edges))
+
+    edge_index = torch.tensor(edges, dtype=torch.long).t().contiguous()
+
+    data = defaultdict(list)
+
+    if G.number_of_nodes() > 0:
+        node_attrs = list(next(iter(G.nodes(data=True)))[-1].keys())
+    else:
+        node_attrs = {}
+
+    if G.number_of_edges() > 0:
+        edge_attrs = list(next(iter(G.edges(data=True)))[-1].keys())
+    else:
+        edge_attrs = {}
+
+    for i, (_, feat_dict) in enumerate(G.nodes(data=True)):
+        if set(feat_dict.keys()) != set(node_attrs):
+            raise ValueError("Not all nodes contain the same attributes")
+        for key, value in feat_dict.items():
+            data[str(key)].append(value)
+
+    for i, (_, _, feat_dict) in enumerate(G.edges(data=True)):
+        if set(feat_dict.keys()) != set(edge_attrs):
+            raise ValueError("Not all edges contain the same attributes")
+        for key, value in feat_dict.items():
+            key = f"edge_{key}" if key in node_attrs else key
+            data[str(key)].append(value)
+
+    for key, value in data.items():
+        try:
+            data[key] = torch.tensor(value)
+        except ValueError:
+            pass
+
+    data["edge_index"] = edge_index.view(2, -1)
+    data = Data.from_dict(data)
+
+    if group_node_attrs is all:
+        group_node_attrs = list(node_attrs)
+    if group_node_attrs is not None:
+        xs = []
+        for key in group_node_attrs:
+            x = data[key]
+            x = x.view(-1, 1) if x.dim() <= 1 else x
+            xs.append(x)
+            del data[key]
+        data.x = torch.cat(xs, dim=-1)
+
+    if group_edge_attrs is all:
+        group_edge_attrs = list(edge_attrs)
+    if group_edge_attrs is not None:
+        xs = []
+        for key in group_edge_attrs:
+            key = f"edge_{key}" if key in node_attrs else key
+            x = data[key]
+            x = x.view(-1, 1) if x.dim() <= 1 else x
+            xs.append(x)
+            del data[key]
+        data.edge_attr = torch.cat(xs, dim=-1)
+
+    if data.x is None and data.pos is None:
+        data.num_nodes = G.number_of_nodes()
+
+    return data
 
 
 class RemoveSelfLoops(pyg.transforms.BaseTransform):
@@ -449,7 +534,7 @@ class RemoveSelfLoops(pyg.transforms.BaseTransform):
         self.device = device
         self.non_blocking = non_blocking
 
-    def __call__(self, data: pyg.data.Data):
+    def __call__(self, data: Data):
         data.edge_index, _ = pyg.utils.remove_self_loops(data.edge_index)
         data = T.RemoveIsolatedNodes()(data)
         return data
